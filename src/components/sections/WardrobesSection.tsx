fa5b8e6a5157749a7acb232065d5862ec31a6d36
Serhii Rud <carmagsales@gmail.com>
2025-05-27 20:44:14 +0200

    swiper size wardropes and other

--- src/components/sections/WardrobesSection.tsx
+++ src/components/sections/WardrobesSection.tsx
@@ -11,7 +11,7 @@ const WardrobesSection = () => {
       <h2 className="pl-48 mb-10 text-white/60 font-bold text-4xl leading-none font-bebas ">
         {sections.wardrobes.title}
       </h2>
-      <div className="mx-auto mb-4 container">
+      <div className="mx-auto px-6">
         <Gallery data={data} />
       </div>
       <div className="ml-auto mr-48 w-[700px] text-xl text-white/60 ">
